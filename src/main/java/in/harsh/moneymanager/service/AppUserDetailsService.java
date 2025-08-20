aee68986821d8e7195a74cb9c4b027667f81d605
Harsh10 <harshsahani078@gmail.com>
2025-08-20 21:22:46 +0530

    Added user service

--- src/main/java/in/harsh/moneymanager/service/AppUserDetailsService.java
+++ src/main/java/in/harsh/moneymanager/service/AppUserDetailsService.java
@@ -1,13 +1,33 @@
 package in.harsh.moneymanager.service;
 
+import in.harsh.moneymanager.entity.ProfileEntity;
+import in.harsh.moneymanager.repositry.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
 public class AppUserDetailsService implements UserDetailsService {
 
+    private final ProfileRepository profileRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        ProfileEntity profileEntity = profileRepository.findByEmail(email).
+                orElseThrow(() -> new UsernameNotFoundException("Profile not found for email" + email));
+
+        return  User.builder().
+                username(profileEntity.getFullName()).
+                password(profileEntity.getPassword()).
+                authorities(Collections.emptyList()).
+                build();
+
     }
 }
